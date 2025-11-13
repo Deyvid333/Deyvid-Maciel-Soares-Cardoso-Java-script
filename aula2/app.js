f70005ea7685b53dae4ab45b8f78c53d54deae04
Deyvid333 <rm94999@estudante.fieb.edu.br>
2025-11-13 14:56:47 -0300

    resolução de mais exercicios

---
+++ aula2/app.js
@@ -0,0 +1,73 @@
+let nome = "astoufo "
+let idade = 36
+
+if(idade >= 18){
+    console.log(nome + "pode tirar a carteira de motorista pois, é maior de idade")
+}else console.log(nome + "não pode tirar a carteira de motorista pois, é menor de idade")
+
+
+
+
+
+
+
+
+
+let numero = 88
+if(numero > 0){
+    console.log(numero + " o numero digitado é positivo")
+}else console.log(numero + " o numero digitado é negativo")
+
+
+
+
+
+
+
+let numero1 = 69
+
+if(numero1 %2 == 0){
+    console.log(" o numero " + numero1 + " é par" )
+}else{
+    console.log(" o numero " + numero1  + " é impar ")
+}
+
+
+
+
+
+
+
+let numero11 = 15
+let numero2 = 12
+const numero3 = (numero11 + numero2) 
+const numero4 = (numero3 / 2)
+
+console.log(" a media dos dois numeros selecionados é: " + numero4)
+
+
+
+
+
+
+
+
+
+
+let A = 14
+let B = 11
+let C = (A - B)
+let D = (B - A)
+
+if(A > B){
+    console.log("o número: " + A + " A " + " é maior que: " + B + " B "  +" o resultado da subtração é: " + C )
+}else if( A < B){
+    console.log("o número: " + B + " B " + " é maior que: " + A + " A " + " o resultado da subtração é: " + D )
+}else{
+    console.log("os números digitados são iguais a subtração dos dois números é 0 ")
+}
+
+
+
+
+
