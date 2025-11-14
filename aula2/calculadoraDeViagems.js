dceb0f73078a766486d996629e609194ef4e12ec
Deyvid333 <rm94999@estudante.fieb.edu.br>
2025-11-14 16:03:04 -0300

    calcular a distancia da viagem e o preço da gasolina a ser gasto para efetuar a ação

--- aula2/calculadoraDeViagems.js
+++ aula2/calculadoraDeViagems.js
@@ -1,7 +1,11 @@
-let tempo = 33
-let velocidade = 80
-let consumo =  20
-let distancia = 500
+let tempo = 9
+let velocidade = 100
+let consumo =  15
+let distancia = (tempo * velocidade)
 let consumoViagem = (distancia / consumo)
+let precoviagem = (consumoViagem * 5.8)
 
-console.log()
+
+
+console.log(` o consumo na viagem em litros de galosina é :  ${consumoViagem.toFixed(3)}   e o preço a ser gasto com a gasolina é de:  ${precoviagem.toFixed(3)}   `)
+console.log(` a distancia a ser percorrida para a efetuação da chegada a seu destino é de :  ${distancia.toFixed(3)} ` )
